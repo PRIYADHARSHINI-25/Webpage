2d8c2f3dd8a79672af3ad8270e0ed4859bbc3ea8
Swetha S <swethassivasankaran@gmail.com>
2022-09-01 17:26:07 +0530

    updated

---
+++ demo/src/components/Products/Products.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Grid from '@material-ui/core'
+
+const products = {
+    { id: 1, name: 
+    }
+}
+
+const Products = () => {
+    <main>
+        <Grid container justify="center" spacing={4}>
+
+        </Grid>
+    </main>
+}
+
+export default Products;
